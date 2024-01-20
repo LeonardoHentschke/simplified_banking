fd398bb38778eca2c8cff0ffee01f782bee5bb9b
Leonardo <leo.hentschke@hotmail.com>
2024-01-20 17:40:09 -0300

    Create User repository

---
+++ src/main/java/com/simplified_banking/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.simplified_banking.repositories;
+
+import com.simplified_banking.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
